4b310baa0524281c113bd0163c46df4c04531792
TheSanvi <sanvivermavns@gmail.com>
2023-07-19 15:14:16 +0530

    changes done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,65 +8,132 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String>profileimages=[
+    "images/1.jpeg",
+    "images/2.jpeg",
+    "images/3.jpeg",
+    "images/4.jpeg",
+    "images/5.jpeg",
+    "images/6.jpeg",
+    "images/7.jpeg",
+    "images/8.jpeg",
+  ];
+  List<String>posts=[
+    "images/post1.jpeg",
+    "images/post2.jpeg",
+    "images/post3.jpeg",
+    "images/post4.jpeg",
+    "images/post5.jpeg",
+    "images/post6.jpeg",
+    "images/post7.jpeg",
+    "images/post8.jpeg",
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Center(
-          child: Text("Calculator",
-            style: TextStyle(fontWeight: FontWeight.bold),),
-        ),
+        title: Image.asset("images/instatitle.png",
+            height: 50),
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.add_circle_outline)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.chat_bubble_outline))
+        ],
 
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      hintText: "Enter Number 1"
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      hintText: "Enter Number 2"
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(onPressed:()=>{}, child: Text("Addition")),
-                    ElevatedButton(onPressed:()=>{}, child: Text("Subtraction")),
+                  children:List.generate(
+                    8,
+                        (index)=>Container(padding:EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            backgroundImage: AssetImage("images/instagrad.jpeg"),
+                            child: CircleAvatar(
+                              radius: 32,
+                              backgroundImage: AssetImage(profileimages[index]),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text("Profile Name",
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.black87
+                            ),),
 
-                  ],
-                ),
-              ),
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+            ),
+            Divider(),
+            Column(
+              children: List.generate(8, (index) => Column(
+
                 children: [
-                  ElevatedButton(onPressed:()=>{}, child: Text("Multiplication")),
-                  ElevatedButton(onPressed:()=>{}, child: Text("Division")),
+                  Row(
+                    children: [
+                      Container(padding: EdgeInsets.all(10),
+                        child: CircleAvatar(
+                          radius: 14,
+                          backgroundImage: AssetImage("images/instagrad.jpeg"),
+                          child: CircleAvatar(
+                            radius: 12,
+                            backgroundImage: AssetImage(profileimages[index]),
+                          ),
+                        ),
 
+                      ),
+                      Text("Profile Name"),
+                      Spacer(),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.more_vert)
+                      ),
+                    ],
+                  ),
+                  Image.asset(posts[index]),
+                  Row(
+                    children: [
+                      IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border),),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.chat_bubble_outline),),
+                      IconButton(onPressed: (){}, icon: Icon(Icons.label_outline),),
+                      Spacer(),IconButton(onPressed: (){}, icon: Icon(Icons.bookmark_border),),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(text: TextSpan(
+                            style: TextStyle(color: Colors.black),
+                            children: [TextSpan(text: "Liked by "),
+                              TextSpan(text: "Profile Name",style:TextStyle(
+                                  fontWeight: FontWeight.bold
+                              ) )
+
+                            ]
+                        ))
+                      ],
+                    ),
+                  )
                 ],
-              )
-            ],
-          ),
+              )),
+            )
+
+
+
+          ],
         ),
       ),
-      backgroundColor: Colors.teal[300],
     );
   }
 }
